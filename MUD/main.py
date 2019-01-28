16eec28a5e7d1a7bc0f23c6b2b19e43d9dd6a02f
Tomas <mazurkevic.tomas@gmail.com>
2019-01-28 15:19:57 +0000

    Added an array of directions and actions and if statements to the main.py

--- MUD/main.py
+++ MUD/main.py
@@ -1,18 +1,21 @@
+actions = ["move", "go", "search", "look"]
+directions = ["up", "down", "right", "left"]
+
 action = input("What is your action? \n")
 
-if action.lower() == "move":
-    move = input("What direction?")
-    if move.lower() == "right":
+if action.lower() == actions[0] or action.lower() == actions[1]:
+    move = input("What direction? \n")
+    if move.lower() == directions[0]:
         print("You entered went to the right room")
-    elif move.lower() == "left":
+    elif move.lower() == directions[1]:
         print("There is no room on the left")
-    elif move.lower() == "up":
+    elif move.lower() == directions[2]:
         print("Entered a room")
-    elif move.lower() == "down":
+    elif move.lower() == directions[3]:
         print("Entered a room")
     else:
         print("Wrong input")
-elif action.lower() == "search":
+elif action.lower() == actions[2] or action.lower() == actions[3]:
     print("Nothing has been found")
 else:
     print("Wrong input")
